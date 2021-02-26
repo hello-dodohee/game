28cf84725dc224d0f54991197f17310db02c05a5
hello-dodohee <73160509+hello-dodohee@users.noreply.github.com>
2021-02-26 22:27:34 +0900

    Created Collision Detection and Scoring

--- draw.js
+++ draw.js
@@ -2,8 +2,12 @@
 const canvas = document.getElementById('pingpong');
 const ctx = canvas.getContext('2d');
 
-// 추가적인 변수
+// 사운드 추가하기
+const hitSound = new Audio('../sounds/hitSound.wav');  // 공이 패들을 치면 소리나게
+const scoreSound = new Audio('../sounds/scoreSound.wav'); // 누군가 점수를 얻으면 소리나게
+const wallHitSound = new Audio('../sounds/wallHitSound.wav');  // 공이 상단이나 하단 벽에 닿으면 소리나게
 
+// 추가적인 변수
 //네트
 const netWidth = 4;
 const netHeight = canvas.height;
@@ -16,8 +20,7 @@ let upArrowPressed = false;
 let downArrowPressed = false;
 
 //객체 만들기 x 위치 , y 위치 , 폭 , 높이 , 컬러
-
-//네트 x 위치, 
+//네트
 const net = {
     x : canvas.width / 2 - netWidth / 2,
     y : 0,
@@ -26,7 +29,7 @@ const net = {
     color: "#fff"
 };
 
-//paddle
+//패들 - 사용자
 const user = {
     x : 10,
     y : canvas.height / 2 - paddleHeight / 2,
@@ -36,6 +39,7 @@ const user = {
     score : 0
 };
 
+// 패들 - 사용자
 const com = {
     x : canvas.width - (paddleWidth + 10),
     y : canvas.height / 2 - paddleHeight / 2,
@@ -45,7 +49,7 @@ const com = {
     score : 0
 };
 
-//ball x 위치, y 위치 반경, 속도, velocityX, velocityY , 컬러
+//공 x 위치, y 위치 반경, 속도, velocityX, velocityY , 컬러
 const ball = {
     x : canvas.width / 2,
     y : canvas.height / 2,
@@ -145,16 +149,89 @@ function update(){
       }
 
     // 공 충돌감지
-    
+    if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0) {
+    // play 벽에 부딪히는 소리
 
-    // 공 움직임
-    //1. 공을 아래쪽으로 움직이게 설정
+    ball.velocityY = -ball.velocityY;
+  }
+
+   // 만약 공이 오른쪽 벽에 부딪힐때
+   if (ball.x + ball.radius >= canvas.width) {
+    // play scoreSound
+
+    // 사용자 스코어가 1점 오른다.
+    user.score += 1;
+    reset();
+  }
+
+  // 만약 공이  왼쪽 벽에 부딪힐때
+  if (ball.x - ball.radius <= 0) {
+    // play scoreSound
+
+    // 컴퓨터 스코어가 1점 오른다.
+    com.score += 1;
+    reset();
+  }
+// 공 자리 리셋시키기
+function reset() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.speed = 7;
+  
+    // 턴마다 공자리를 바꾼다.
+    ball.velocityX = -ball.velocityX;
+    ball.velocityY = -ball.velocityY;
+  }
+
+// 공과 패들 충돌감지
+// 공이 패들에 닿으면 true / 그렇지 않으면 false 반환
+function collisionDetect(player, ball) {
+    // returns true or false
+    player.top = player.y;
+    player.right = player.x + player.width;
+    player.bottom = player.y + player.height;
+    player.left = player.x;
+  
+    ball.top = ball.y - ball.radius;
+    ball.right = ball.x + ball.radius;
+    ball.bottom = ball.y + ball.radius;
+    ball.left = ball.x - ball.radius;
+  
+    return ball.left < player.right && ball.top < player.bottom && ball.right > player.left && ball.bottom > player.top;
+  }
+
+    // 공 움직임 기본셋팅 - 공을 아래쪽으로 움직이게 설정
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
 
     // 컴퓨터 패들 움직임
-
+    com.y += ((ball.y - (com.y + com.height / 2))) * 0.09;
     //
+
+let player = (ball.x < canvas.width / 2) ? user : com;
+
+  if (collisionDetect(player, ball)) {
+    // 부딪히는 소리
+
+    // 각도는 0으로 기본셋팅
+    let angle = 0;
+
+    // 공이 패들 상단을 치면 -45도 각도로 반사된다.
+    if (ball.y < (player.y + player.height / 2)) {
+      angle = -1 * Math.PI / 4;
+    } else if (ball.y > (player.y + player.height / 2)) {
+      // 공이 패들 하단을 치면 45도 각도로 반사된다.
+      angle = Math.PI / 4;
+    }
+
+    // 공이 사용자 패들에 부딪히면 오른쪽으로 밀고,
+    // 공이 컴퓨터 패들에 부딪히면 왼쪽으로 민다.
+    ball.velocityX = (player === user ? 1 : -1) * ball.speed * Math.cos(angle);
+    ball.velocityY = ball.speed * Math.sin(angle);
+
+    // 공 속도 증가
+    ball.speed += 0.2;
+  }
 }
 
 
